92974110d7d7f68ea47abbd61e888d04ab99fcb8
PraisesPJMT <praisesmusa@outlook.com>
2022-08-10 11:26:56 +0100

    Implement function to add new task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const todayList = document.querySelector('.list');
+const todayListBox = document.querySelector('.list');
+const addList = document.querySelector('.fa-check-to-slot');
+const listInput = document.querySelector('input[name="task-input"]');
 const listCatalogue = [
   {
     index: 4,
@@ -22,10 +24,24 @@ const listCatalogue = [
   },
 ];
 
+class Task {
+  constructor(description) {
+    this.description = description;
+    this.complete = false;
+    this.index = null;
+  }
+
+  createTask = (index, complete, description) => {
+    listCatalogue.push({ index, complete, description });
+  }
+}
+
+const todayList = new Task();
+
 const renderCatalogue = () => {
   listCatalogue.sort((a, b) => a.index - b.index);
   listCatalogue.forEach((item) => {
-    todayList.innerHTML += `<li class="list-item">
+    todayListBox.innerHTML += `<li class="list-item">
                     <div class="task-display">
                         <input type="checkbox" name="tasks" class="checkbox">
                         <input type="text" name="tasks-item" value="${item.description}" class="task-item" id="Task ${item.index}">
@@ -37,4 +53,38 @@ const renderCatalogue = () => {
                 </li>`;
   });
 };
+
+const addToDoItem = (item) => {
+  const complete = false;
+  const index = listCatalogue.length + 1;
+  todayList.createTask(index, complete, item);
+  todayListBox.innerHTML += `<li class="list-item">
+                    <div class="task-display">
+                        <input type="checkbox" name="tasks" class="checkbox">
+                        <input type="text" name="tasks-item" value="${item}" class="task-item" id="Task ${index}">
+                    </div>
+                    <div class="task-control">
+                        <i class="fa-solid fa-trash-can"></i>
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </div>
+                </li>`;
+  document.querySelector('input[name="task-input"]').value = '';
+};
+
+addList.addEventListener('click', () => {
+  const toDo = listInput.value;
+  if (toDo.length > 0) {
+    addToDoItem(toDo);
+  }
+});
+
+listInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    const toDo = listInput.value;
+    if (toDo.length > 0) {
+      addToDoItem(toDo);
+    }
+  }
+});
+
 renderCatalogue();
