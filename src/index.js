d16b9abc114d0d67c4b6a6cfb596d8c1707c2ced
PraisesPJMT <praisesmusa@outlook.com>
2022-08-10 15:34:10 +0100

    Implement function to remove task

--- src/index.js
+++ src/index.js
@@ -34,6 +34,13 @@ class Task {
   createTask = (index, complete, description) => {
     listCatalogue.push({ index, complete, description });
   }
+
+  removeTask = (item) => {
+    const task = item.querySelector('input[type="text"]').value;
+    const filt = listCatalogue.filter((listItem) => task === listItem.description);
+    const filtTask = listCatalogue.indexOf(filt[0]);
+    listCatalogue.splice(filtTask, 1);
+  }
 }
 
 const todayList = new Task();
@@ -46,10 +53,8 @@ const renderCatalogue = () => {
                         <input type="checkbox" name="tasks" class="checkbox">
                         <input type="text" name="tasks-item" value="${item.description}" class="task-item" id="Task ${item.index}">
                     </div>
-                    <div class="task-control">
                         <i class="fa-solid fa-trash-can"></i>
-                        <i class="fa-solid fa-ellipsis-vertical"></i>
-                    </div>
+                        <i class="fa-solid func fa-ellipsis-vertical"></i>
                 </li>`;
   });
 };
@@ -63,12 +68,26 @@ const addToDoItem = (item) => {
                         <input type="checkbox" name="tasks" class="checkbox">
                         <input type="text" name="tasks-item" value="${item}" class="task-item" id="Task ${index}">
                     </div>
-                    <div class="task-control">
                         <i class="fa-solid fa-trash-can"></i>
-                        <i class="fa-solid fa-ellipsis-vertical"></i>
-                    </div>
+                        <i class="fa-solid func fa-ellipsis-vertical"></i>
                 </li>`;
   document.querySelector('input[name="task-input"]').value = '';
+};
+
+const taskFunction = (item) => {
+  const func = item.querySelector('.func');
+  const del = item.querySelector('.fa-trash-can');
+  func.style.display = 'none';
+  del.style.display = 'block';
+};
+
+const resetlist = () => {
+  todayListBox.querySelectorAll('.func').forEach((button) => {
+    button.style.display = 'block';
+  });
+  todayListBox.querySelectorAll('.fa-trash-can').forEach((button) => {
+    button.style.display = 'none';
+  });
 };
 
 addList.addEventListener('click', () => {
@@ -84,6 +103,18 @@ listInput.addEventListener('keypress', (event) => {
     if (toDo.length > 0) {
       addToDoItem(toDo);
     }
+  }
+});
+
+todayListBox.addEventListener('click', (event) => {
+  if (event.target.classList.contains('func')) {
+    const item = event.target.parentElement;
+    resetlist();
+    taskFunction(item);
+  } else if (event.target.classList.contains('fa-trash-can')) {
+    const item = event.target.parentElement;
+    todayList.removeTask(item);
+    todayListBox.removeChild(item);
   }
 });
 
