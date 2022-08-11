b66810aa1d12f08afbe2d1df4a36ac7610ce8584
PraisesPJMT <praisesmusa@outlook.com>
2022-08-11 08:22:03 +0100

    Implement function to edit task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const todayListBox = document.querySelector('.list');
 const addList = document.querySelector('.fa-check-to-slot');
 const listInput = document.querySelector('input[name="task-input"]');
+const taskInput = document.querySelectorAll('input');
 const listCatalogue = [
   {
     index: 4,
@@ -51,7 +52,7 @@ const renderCatalogue = () => {
     todayListBox.innerHTML += `<li class="list-item">
                     <div class="task-display">
                         <input type="checkbox" name="tasks" class="checkbox">
-                        <input type="text" name="tasks-item" value="${item.description}" class="task-item" id="Task ${item.index}">
+                        <input type="text" name="tasks-item" value="${item.description}" class="task-item" id="task-${item.index}" readonly>
                     </div>
                         <i class="fa-solid fa-trash-can"></i>
                         <i class="fa-solid func fa-ellipsis-vertical"></i>
@@ -66,7 +67,7 @@ const addToDoItem = (item) => {
   todayListBox.innerHTML += `<li class="list-item">
                     <div class="task-display">
                         <input type="checkbox" name="tasks" class="checkbox">
-                        <input type="text" name="tasks-item" value="${item}" class="task-item" id="Task ${index}">
+                        <input type="text" name="tasks-item" value="${item}" class="task-item" id="task-${index}" readonly>
                     </div>
                         <i class="fa-solid fa-trash-can"></i>
                         <i class="fa-solid func fa-ellipsis-vertical"></i>
@@ -74,19 +75,61 @@ const addToDoItem = (item) => {
   document.querySelector('input[name="task-input"]').value = '';
 };
 
-const taskFunction = (item) => {
-  const func = item.querySelector('.func');
-  const del = item.querySelector('.fa-trash-can');
-  func.style.display = 'none';
-  del.style.display = 'block';
+const resetColors = () => {
+  const listItems = document.querySelectorAll('li');
+  const inputs = document.querySelectorAll('input');
+  listItems.forEach((item) => {
+    item.style.backgroundColor = '#fff';
+  });
+  inputs.forEach((input) => {
+    input.style.backgroundColor = '#fff';
+  });
 };
 
-const resetlist = () => {
+const resetList = () => {
   todayListBox.querySelectorAll('.func').forEach((button) => {
     button.style.display = 'block';
   });
   todayListBox.querySelectorAll('.fa-trash-can').forEach((button) => {
     button.style.display = 'none';
+  });
+};
+
+const taskFunction = (item) => {
+  const func = item.querySelector('.func');
+  const del = item.querySelector('.fa-trash-can');
+  const input = item.querySelector('input[name="tasks-item"]');
+  const inputValue = input.value;
+  let ident;
+  item.style.backgroundColor = '#fcf299';
+  input.style.backgroundColor = '#fcf299';
+  func.style.display = 'none';
+  del.style.display = 'block';
+  input.removeAttribute('readonly');
+  input.focus();
+  listCatalogue.forEach((task) => {
+    if (task.description === inputValue) {
+      ident = task.index;
+    }
+  });
+  input.addEventListener('keydown', () => {
+    listCatalogue.forEach((task) => {
+      if (task.index === ident) {
+        task.description = item.querySelector('input[name="tasks-item"]').value;
+      }
+    });
+  });
+  input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      listCatalogue.forEach((task) => {
+        if (task.index === ident) {
+          task.description = item.querySelector('input[name="tasks-item"]').value;
+          input.setAttribute('readonly', 'readonly');
+          resetList();
+          resetColors();
+        }
+      });
+    }
   });
 };
 
@@ -98,6 +141,7 @@ addList.addEventListener('click', () => {
 });
 
 listInput.addEventListener('keypress', (event) => {
+  resetColors();
   if (event.key === 'Enter') {
     const toDo = listInput.value;
     if (toDo.length > 0) {
@@ -106,10 +150,23 @@ listInput.addEventListener('keypress', (event) => {
   }
 });
 
+listInput.addEventListener('click', () => {
+  resetList();
+  resetColors();
+});
+
+taskInput.forEach((input) => {
+  input.addEventListener('click', () => {
+    resetList();
+    resetColors();
+  });
+});
+
 todayListBox.addEventListener('click', (event) => {
   if (event.target.classList.contains('func')) {
     const item = event.target.parentElement;
-    resetlist();
+    resetList();
+    resetColors();
     taskFunction(item);
   } else if (event.target.classList.contains('fa-trash-can')) {
     const item = event.target.parentElement;
